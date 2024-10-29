744ed1ad4d70ecc139e665ad8eb59d1be7bd4aa3
Mark Velesaca <mark.velesaca@gmail.com>
2024-10-29 09:43:02 -0500

    Added a home page only for logged in users, might change in the future

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,128 @@
+// import React, { useEffect } from 'react';
+// import {
+//   HashRouter as Router,
+//   Redirect,
+//   Route,
+//   Switch,
+// } from 'react-router-dom';
+
+// import { useDispatch, useSelector } from 'react-redux';
+
+// import Nav from '../Nav/Nav';
+// import Footer from '../Footer/Footer';
+
+// import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+
+// import AboutPage from '../AboutPage/AboutPage';
+// import UserPage from '../UserPage/UserPage';
+// import InfoPage from '../InfoPage/InfoPage';
+// import LandingPage from '../LandingPage/LandingPage';
+// import LoginPage from '../LoginPage/LoginPage';
+// import RegisterPage from '../RegisterPage/RegisterPage';
+// import Home from '../HomePage/HomePage'
+// import './App.css';
+
+// function App() {
+//   const dispatch = useDispatch();
+
+//   const user = useSelector(store => store.user);
+
+//   useEffect(() => {
+//     dispatch({ type: 'FETCH_USER' });
+//   }, [dispatch]);
+
+//   return (
+//     <Router>
+//       <div>
+//         <Nav />
+//         <Switch>
+//           {/* Visiting localhost:5173 will redirect to localhost:5173/home */}
+//           <Redirect exact from="/" to="/home" />
+
+//           {/* Visiting localhost:5173/about will show the about page. */}
+//           <Route
+//             // shows AboutPage at all times (logged in or not)
+//             exact
+//             path="/about"
+//           >
+//             <AboutPage />
+//           </Route>
+
+//           {/* For protected routes, the view could show one of several things on the same route.
+//             Visiting localhost:5173/user will show the UserPage if the user is logged in.
+//             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
+//             Even though it seems like they are different pages, the user is always on localhost:5173/user */}
+//           <ProtectedRoute
+//             // logged in shows UserPage else shows LoginPage
+//             exact
+//             path="/user"
+//           >
+//             <UserPage />
+//           </ProtectedRoute>
+
+//           <ProtectedRoute
+//             // logged in shows InfoPage else shows LoginPage
+//             exact
+//             path="/info"
+//           >
+//             <InfoPage />
+//           </ProtectedRoute>
+
+//           <Route
+//             exact
+//             path="/login"
+//           >
+//             {user.id ?
+//               // If the user is already logged in, 
+//               // redirect to the /user page
+//               <Redirect to="/user" />
+//               :
+//               // Otherwise, show the login page
+//               <LoginPage />
+//             }
+//           </Route>
+
+//           <Route
+//             exact
+//             path="/registration"
+//           >
+//             {user.id ?
+//               // If the user is already logged in, 
+//               // redirect them to the /user page
+//               <Redirect to="/user" />
+//               :
+//               // Otherwise, show the registration page
+//               <RegisterPage />
+//             }
+//           </Route>
+
+//           <Route
+//             exact
+//             path="/home"
+//           >
+//             {user.id ?
+//               // If the user is already logged in, 
+//               // redirect them to the /user page
+//               <Redirect to="/user" />
+//               :
+//               // Otherwise, show the Landing page
+//               <LandingPage />
+//             }
+//           </Route>
+
+//           {/* If none of the other routes matched, we will show a 404. */}
+//           <Route>
+//             <h1>404</h1>
+//           </Route>
+//         </Switch>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import React, { useEffect } from 'react';
 import {
   HashRouter as Router,
@@ -19,7 +144,7 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
-
+import Home from '../HomePage/HomePage'; // Ensure this import path is correct
 import './App.css';
 
 function App() {
@@ -39,21 +164,14 @@ function App() {
           {/* Visiting localhost:5173 will redirect to localhost:5173/home */}
           <Redirect exact from="/" to="/home" />
 
-          {/* Visiting localhost:5173/about will show the about page. */}
           <Route
-            // shows AboutPage at all times (logged in or not)
             exact
             path="/about"
           >
             <AboutPage />
           </Route>
 
-          {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:5173/user will show the UserPage if the user is logged in.
-            If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
-            Even though it seems like they are different pages, the user is always on localhost:5173/user */}
           <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
             exact
             path="/user"
           >
@@ -61,7 +179,6 @@ function App() {
           </ProtectedRoute>
 
           <ProtectedRoute
-            // logged in shows InfoPage else shows LoginPage
             exact
             path="/info"
           >
@@ -73,11 +190,8 @@ function App() {
             path="/login"
           >
             {user.id ?
-              // If the user is already logged in, 
-              // redirect to the /user page
               <Redirect to="/user" />
               :
-              // Otherwise, show the login page
               <LoginPage />
             }
           </Route>
@@ -87,25 +201,20 @@ function App() {
             path="/registration"
           >
             {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
               <Redirect to="/user" />
               :
-              // Otherwise, show the registration page
               <RegisterPage />
             }
           </Route>
 
+          {/* Home Page Route */}
           <Route
             exact
             path="/home"
           >
             {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
+              <Home /> // Render Home if the user is logged in
               :
-              // Otherwise, show the Landing page
               <LandingPage />
             }
           </Route>
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// import AnimalList from './AnimalList';
+
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to Guinea Pig & CritterCraze</h1>
+        <p>Your one-stop shop for all types of critters and guinea pigs!</p>
+      </header>
+      
+      <section>
+        <h2>Available Animals</h2>
+        {/* <AnimalList /> */}
+      </section>
+    </div>
+  );
+};
+
+export default Home;
