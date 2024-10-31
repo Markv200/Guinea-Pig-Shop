0ca310ed59cabb4c8c65dd349330910a2125fc3a
Mark Velesaca <mark.velesaca@gmail.com>
2024-10-31 14:39:23 -0500

    fixed my nav jsx to style it the way i want

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,36 +9,40 @@ function Nav() {
 
   return (
     <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Guinea Pig Shoppe</h2>
-      </Link>
-      <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
+      {/* Left Side - "Guinea Pig Shoppe" title and Shop link */}
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Link to="/home">
+          <h2 className="nav-title">Guinea Pig Shoppe</h2>
+        </Link>
+        <Link to="/shop" className="navLink">
+          Shop
+        </Link>
+      </div>
+
+      {/* Right Side - Links and User Dropdown */}
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Link to="/checkout" className="navLink">
+          Checkout
+        </Link>
+        <Link to="/about" className="navLink">
+          About
+        </Link>
+
+        {/* Conditional Links based on user authentication */}
+        {!user.id ? (
           <Link className="navLink" to="/login">
             Login / Register
           </Link>
+        ) : (
+          <div className="user-dropdown">
+            <button className="navLink">
+              {user.username}
+            </button>
+            <div className="dropdown-content">
+              <LogOutButton className="logout-button" />
+            </div>
+          </div>
         )}
-
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
       </div>
     </div>
   );
