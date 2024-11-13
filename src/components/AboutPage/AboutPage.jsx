5d12836e642c165a8f1435a744d774c90eaf3897
Mark Velesaca <mark.velesaca@gmail.com>
2024-11-13 09:53:50 -0600

    changed checkout

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './About.css'
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
@@ -9,15 +9,16 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-      <p>Guinea Pig Shoppe is a website for customers interested in purchasing a unique variety of guinea pigs. Whether you're looking for a cuddly pet or your next meal, Guinea Pig Shoppe has you covered. We offer a selection of guinea pigs in various stages of life from babies and pregnant moms to be to adult males and females, each available in different colors, patterns, and ages. For those with culinary interests, we also provide cleaned, deceased guinea pigs ready for cooking. Explore our diverse options to find exactly what you need!</p>
-     <h1>Contact Us</h1>
-      <p>(952)463-0233</p>  
-      <p>What I used in my Code</p>  
+      {/* <p>Guinea Pig Shoppe is a website for customers interested in purchasing a unique variety of guinea pigs. Whether you're looking for a cuddly pet or your next meal, Guinea Pig Shoppe has you covered. We offer a selection of guinea pigs in various stages of life from babies and pregnant moms to be to adult males and females, each available in different colors, patterns, and ages. For those with culinary interests, we also provide cleaned, deceased guinea pigs ready for cooking. Explore our diverse options to find exactly what you need!</p> */}
+     {/* <h1>Contact Us</h1> */}
+      {/* <p>(952)463-0233</p>   */}
+      <p>Technologies Used</p>  
         <tr>Redux</tr>
         <tr>Redux Saga</tr>
-        <tr></tr>
-        <tr></tr>
-        <tr></tr>
+        <tr>Node</tr>
+        <tr>Express</tr>
+        <tr>React</tr>
+        <tr>Passport</tr>
 
       </div>
     </div>
@@ -25,3 +26,4 @@ function AboutPage() {
 }
 
 export default AboutPage;
+
